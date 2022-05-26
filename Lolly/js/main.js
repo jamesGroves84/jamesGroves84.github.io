b221dcc4727e203c3549fb1f9f1fe48897c89e02
jamesGroves84 <Delirious2529@outlook.com>
2022-05-26 10:38:14 +0100

    Testing readMultipleFiles function

--- Lolly/js/main.js
+++ Lolly/js/main.js
@@ -13,6 +13,23 @@ window.onload = () => {
 document.getElementById('imgPicker').addEventListener('change', readMultipleFiles, false);
 
 function readMultipleFiles(evt) {
+ //Retrieve all the files from the FileList object
+ var files = evt.target.files; 
 
+ if (files) {
+     for (var i=0, f; f=files[i]; i++) {
+           var r = new FileReader();
+         r.onload = (function(f) {
+             return function(e) {
+                 var contents = e.target.result;
+                 $('body').append('<h2>' + f.name + '</h2><img src="learn/' + f.name + '"/>');
+             };
+         })(f);
+
+         r.readAsText(f);
+     }   
+ } else {
+       alert("Failed to load files"); 
+ }
 }
 //
