ebbd048442d023a09aa930b3d8c0a9823de3610a
jamesGroves84 <Delirious2529@outlook.com>
2022-05-26 12:02:22 +0100

    debugging Carousel 1st image not set as active

--- Lolly/js/main.js
+++ Lolly/js/main.js
@@ -18,11 +18,12 @@ function getImgs() {
     var imgs = document.getElementById('imgPicker').files;
 
     for(var i = 0; i < imgs.length; i++) {
-        var isActive = (i == 0) ? ' active ' : '';
+        var firstImg = (i == 0) ? ' active ' : '';
+        console.log(firstImg +  'on ' + imgs[i].name);
         const imageReader = new FileReader();
         imageReader.readAsDataURL(imgs[i]);
         imageReader.addEventListener('load', function() {
-            document.getElementById('imageFrame').innerHTML += '<div class="carousel-item '+isActive+'"><img class="img-fluid d-block w-100" src="'+this.result+'" />';
+            document.getElementById('imageFrame').innerHTML += '<div class="carousel-item '+firstImg+'"><img class="img-fluid d-block w-100" src="'+this.result+'" />';
         });
     };
 };
